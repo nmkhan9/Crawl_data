f8284466609d4aa4950886df72f465a12144e293
khang <nguyenmanhkhanghd@gmail.com>
2025-06-15 23:40:56 +0700

    crawl Dynamic website in 99

--- 99.Test.py
+++ 99.Test.py
@@ -1,85 +1,45 @@
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-products = []
 
-for i in range (1,10):
-    response = requests.get(f'https://clickbuy.com.vn/dien-thoai?page={i}')
-    soup = BeautifulSoup(response.content, "html.parser")
-    items = soup.find_all('div',class_='list-products__item')
-    for item in items :
+api_url = "https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&version=home-persionalized&trackity_id=454e679b-6512-b521-394a-35ece2d402da&urlKey=nha-sach-tiki&category=8322&page={}"
 
-        a_tag = item.find('a')
-        link0 = 'https://clickbuy.com.vn' + a_tag['href']
+def get_url(link):
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    response = requests.get(link, headers=headers)
+    return response
 
+all_books = []
 
-        response1 = requests.get(link0)
-        soup1 = BeautifulSoup(response1.content, "html.parser")
-        related_versions = soup1.find_all('div', class_='related_versions__item')
-        version_links = []
+for page in range(1, 2):
+    url = api_url.format(page)
+    tiki1 = get_url(url)
+    data = tiki1.json()
+    products = data.get("data", [])
 
-        for version in related_versions:
-            a_tag = version.find('a')
-            onclick = a_tag.get('onclick')
-            if onclick:
-                link1 = onclick.split("'")[1]
-                full_link = 'https://clickbuy.com.vn' + link1
-                version_links.append(full_link)
+    for i,product in enumerate( products):
+        id_book = product["id"]
+        name_book = product["name"]
+        link_book = f"https://tiki.vn/api/v2/products/{id_book}"
 
+        sp = get_url(link_book)
+        details = sp.json()
 
-        for url in version_links:
-            response2 = requests.get(url)
-            soup2 = BeautifulSoup(response2.content, "html.parser")
-
-            product = {}
-
-            name_tag = soup2.find('h1', class_="product-name")
-            product_name = name_tag.get_text(strip=True) if name_tag else None
-
-            price_new_tag = soup2.find("p", class_="price")
-            price_new_text = price_new_tag.get_text(strip=True) if price_new_tag else None
-
-            price_old_tag = soup2.find("p", class_="price-old")
-            price_old_text = price_old_tag.get_text(strip=True) if price_old_tag else None
+        info = details.get('specifications', [])
+        attributes = info[0].get('attributes', []) if info else []
 
 
+        book_info = {"Name": name_book}
+        for attr in attributes:
+            book_info[attr['name']] = attr['value']
 
-            product = {
-                'Product_Name': product_name,
-                'Product_Link': url,
-                'New_Price': price_new_text,
-                'Old_Price': price_old_text
-            }
+        all_books.append(book_info)
 
-
-            details = soup2.find_all('tbody')
-            if details:
-                fields = {
-                    "screen_size": "Kích thước màn hình",
-                    "cpu": "CPU",
-                    "os": "Hệ điều hành",
-                    "internal_storage": "Bộ nhớ trong",
-                    "ram": "RAM",
-                    "battery_capacity": "Dung lượng pin",
-                    "color": "Màu sắc",
-                    "product_condition": "Tình trạng SP",
-                    "brand": "Hãng sản xuất",
-                    "main_camera": "Camera chính",
-                    "selfie_camera": "Camera phụ",
-                    "resolution": "Độ phân giải màn hình",
-                    "fast_charging": "Sạc nhanh"
-                }
-
-                table = details[-1]
-                for key, label in fields.items():
-                    tag = table.find('th', string=label)
-                    value = tag.find_next('td').get_text(strip=True) if tag else None
-                    product[key] = value
-
-            products.append(product)
+        if i == 10 :
+            break
 
 
-df = pd.DataFrame(products)
-df = df.drop_duplicates(subset=['Product_Name', 'Product_Link'], keep='first')
-
-df.to_csv('99.testtest.csv', index=False, encoding='utf-8-sig')
+for book in all_books:
+    print("=" * 50)
+    for key, value in book.items():
+        print(f"{key}: {value}")
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import requests
+import time
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+base_url = "https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&version=home-persionalized&trackity_id=454e679b-6512-b521-394a-35ece2d402da&urlKey=nha-sach-tiki&category=8322&page={}"
+
+total = 0
+
+for page in range(1,2):
+    url = base_url.format(page)
+    response = requests.get(url, headers=headers)
+
+    data = response.json()
+    products = data.get("data", [])
+
+product=products[0]
+
+ad_list = product.get('advertisement', {}).get('ad', [])
+if ad_list:
+    props = ad_list[0].get('properties', {})
+    print(props['url'])
+else:
+    print("Không có dữ liệu quảng cáo.")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
