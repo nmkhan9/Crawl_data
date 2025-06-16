b2e3ef65fc310a9c0a0bae89530e013586a47bdc
khang <nguyenmanhkhanghd@gmail.com>
2025-06-16 14:30:03 +0700

    update file mobile cellphones

--- 05.Cellphones.py
+++ 05.Cellphones.py
@@ -2,38 +2,119 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+data_base = []
 
-response = requests.get('https://cellphones.com.vn/mobile.html')
-soup = BeautifulSoup(response.content, "html.parser")
+def get_url(link):
+    response = requests.get(link, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup
 
-items = soup.findAll('div',class_='product-info-container product-item')
-data =[]
-for item in items :
-    a_tag = item.find('div',class_='product-info').find('a')
-    link =a_tag['href']
-    price_show = a_tag.find('p',class_='product__price--show').get_text(strip=True)
-    price_tag = a_tag.find('p', class_='product__price--through')
-    if price_tag:
-        price_through = price_tag.get_text(strip=True)
-    else:
-        price_through = None
-    deal_tag = a_tag.find('p', class_='product__price--percent-detail')
-    if deal_tag :
-        deal = deal_tag.get_text(strip=True)
-    else :
-        deal=None
-    name = a_tag.find('h3').get_text(strip=True)
-
-    product = {
-        'Name':name,
-        'Price' : price_show,
-        'Original price' : price_through,
-        'Deal' : deal,
-        'Link' : link
-
+def make_payload(page):
+    return {
+        "query": """
+            query GetProductsByCateId {
+                products(
+                    filter: {
+                        static: {
+                            categories: ["3"],
+                            province_id: 30,
+                            stock: { from: 0 },
+                            stock_available_id: [46, 56, 152, 4920],
+                            filter_price: { from: 0 to: 50000000 }
+                        },
+                        dynamic: {}
+                    },
+                    page: %d,
+                    size: 20,
+                    sort: [{ view: desc }]
+                ) {
+                    general {
+                        name
+                        manufacturer
+                        sku
+                        url_path
+                    }
+                    filterable {
+                        price
+                        special_price
+                        thumbnail
+                    }
+                }
+            }
+        """ % page,
+        "variables": {}
     }
-    data.append(product)
 
-df=pd.DataFrame(data)
-#df.to_csv('mobile_cellphones.csv', index=False, encoding='utf-8-sig')
-print(len(df))
+url = "https://api.cellphones.com.vn/v2/graphql/query"
+headers = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0"
+}
+
+page = 1
+crawled_links = set()
+
+while page <= 10:
+    res = requests.post(url, headers=headers, json=make_payload(page))
+    data = res.json()
+    items = data["data"]["products"]
+
+    for item in items:
+        link_item = "https://cellphones.com.vn/" + item["general"]["url_path"]
+        soup_product = get_url(link_item)
+
+        details = soup_product.find("div", class_="list-linked")
+        if not details:
+            continue
+
+        abcs = details.find_all("a")
+        if not abcs:
+            continue
+
+        for prodcut in abcs:
+            link_in4 = "https://cellphones.com.vn" + prodcut["href"]
+
+            if link_in4 in crawled_links:
+                continue
+            crawled_links.add(link_in4)
+
+            soup_detail = get_url(link_in4)
+
+            title_tag = soup_detail.find("div", class_="box-product-name")
+            title = title_tag.find("h1").get_text(strip=True) if title_tag else None
+
+            new_price_tag = soup_detail.find("div", class_="sale-price")
+            new_price = new_price_tag.get_text(strip=True) if new_price_tag else None
+
+            old_price_tag = soup_detail.find("del", class_="base-price")
+            old_price = old_price_tag.get_text(strip=True) if old_price_tag else None
+
+            rate_tag = soup_detail.find("span", class_="total-rating")
+            rate = rate_tag.get_text(strip=True) if rate_tag else None
+
+            ls_sp = {
+                "Name": title,
+                "Link": link_in4,
+                "New_price": new_price,
+                "Old_price": old_price,
+                "Review": rate
+            }
+
+            details_sp = soup_detail.find_all("tr", class_="technical-content-item")
+            for sp in details_sp:
+                key_tag = sp.find("td")
+                value_tag = key_tag.find_next("td") if key_tag else None
+
+                if key_tag and value_tag:
+                    key = key_tag.get_text(strip=True)
+                    value_p = value_tag.find("p")
+                    value = value_p.get_text(strip=True) if value_p else value_tag.get_text(strip=True)
+                    ls_sp[key] = value
+
+            data_base.append(ls_sp)
+
+    page += 1
+
+df = pd.DataFrame(data_base)
+df.to_csv('05.mobile_cellphones.csv', index=False, encoding='utf-8-sig')
+print("Doneeeee!")
--- 99.Test.py
+++ 99.Test.py
@@ -1,45 +1,120 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-api_url = "https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&version=home-persionalized&trackity_id=454e679b-6512-b521-394a-35ece2d402da&urlKey=nha-sach-tiki&category=8322&page={}"
+data_base = []
 
 def get_url(link):
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
     response = requests.get(link, headers=headers)
-    return response
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup
 
-all_books = []
+def make_payload(page):
+    return {
+        "query": """
+            query GetProductsByCateId {
+                products(
+                    filter: {
+                        static: {
+                            categories: ["3"],
+                            province_id: 30,
+                            stock: { from: 0 },
+                            stock_available_id: [46, 56, 152, 4920],
+                            filter_price: { from: 0 to: 50000000 }
+                        },
+                        dynamic: {}
+                    },
+                    page: %d,
+                    size: 20,
+                    sort: [{ view: desc }]
+                ) {
+                    general {
+                        name
+                        manufacturer
+                        sku
+                        url_path
+                    }
+                    filterable {
+                        price
+                        special_price
+                        thumbnail
+                    }
+                }
+            }
+        """ % page,
+        "variables": {}
+    }
 
-for page in range(1, 2):
-    url = api_url.format(page)
-    tiki1 = get_url(url)
-    data = tiki1.json()
-    products = data.get("data", [])
+url = "https://api.cellphones.com.vn/v2/graphql/query"
+headers = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0"
+}
 
-    for i,product in enumerate( products):
-        id_book = product["id"]
-        name_book = product["name"]
-        link_book = f"https://tiki.vn/api/v2/products/{id_book}"
+page = 1
+crawled_links = set()
 
-        sp = get_url(link_book)
-        details = sp.json()
+while page <= 10:
+    res = requests.post(url, headers=headers, json=make_payload(page))
+    data = res.json()
+    items = data["data"]["products"]
 
-        info = details.get('specifications', [])
-        attributes = info[0].get('attributes', []) if info else []
+    for item in items:
+        link_item = "https://cellphones.com.vn/" + item["general"]["url_path"]
+        soup_product = get_url(link_item)
 
+        details = soup_product.find("div", class_="list-linked")
+        if not details:
+            continue
 
-        book_info = {"Name": name_book}
-        for attr in attributes:
-            book_info[attr['name']] = attr['value']
+        abcs = details.find_all("a")
+        if not abcs:
+            continue
 
-        all_books.append(book_info)
+        for prodcut in abcs:
+            link_in4 = "https://cellphones.com.vn" + prodcut["href"]
 
-        if i == 10 :
-            break
+            if link_in4 in crawled_links:
+                continue
+            crawled_links.add(link_in4)
 
+            soup_detail = get_url(link_in4)
 
-for book in all_books:
-    print("=" * 50)
-    for key, value in book.items():
-        print(f"{key}: {value}")
+            title_tag = soup_detail.find("div", class_="box-product-name")
+            title = title_tag.find("h1").get_text(strip=True) if title_tag else None
+
+            new_price_tag = soup_detail.find("div", class_="sale-price")
+            new_price = new_price_tag.get_text(strip=True) if new_price_tag else None
+
+            old_price_tag = soup_detail.find("del", class_="base-price")
+            old_price = old_price_tag.get_text(strip=True) if old_price_tag else None
+
+            rate_tag = soup_detail.find("span", class_="total-rating")
+            rate = rate_tag.get_text(strip=True) if rate_tag else None
+
+            ls_sp = {
+                "Name": title,
+                "Link": link_in4,
+                "New_price": new_price,
+                "Old_price": old_price,
+                "Review": rate
+            }
+
+            details_sp = soup_detail.find_all("tr", class_="technical-content-item")
+            for sp in details_sp:
+                key_tag = sp.find("td")
+                value_tag = key_tag.find_next("td") if key_tag else None
+
+                if key_tag and value_tag:
+                    key = key_tag.get_text(strip=True)
+                    value_p = value_tag.find("p")
+                    value = value_p.get_text(strip=True) if value_p else value_tag.get_text(strip=True)
+                    ls_sp[key] = value
+
+            data_base.append(ls_sp)
+
+    page += 1
+
+df = pd.DataFrame(data_base)
+df.to_csv('99.testtest.csv', index=False, encoding='utf-8-sig')
+print("Doneeeee!")
